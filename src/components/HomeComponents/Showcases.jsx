67cbba1b6f62c21ac2478d5e9247f5d804888ef1
Wisdom Ochei <wisdomofgod2003@gmail.com>
2023-06-24 00:18:35 +0100

    Putting showcase section back

--- src/components/HomeComponents/Showcases.jsx
+++ src/components/HomeComponents/Showcases.jsx
@@ -1,32 +1,32 @@
 // Image
-// import apartment1 from "../../assets/img/apartment1.png";
-// import apartment2 from "../../assets/img/apartment2.png";
-// //  Icons
-// import whiteArrow from "../../assets/icons/whiteArrow.svg";
-// import blackArrow from "../../assets/icons/blackArrow.svg";
+import apartment1 from "../../assets/img/apartment1.png";
+import apartment2 from "../../assets/img/apartment2.png";
+//  Icons
+import whiteArrow from "../../assets/icons/whiteArrow.svg";
+import blackArrow from "../../assets/icons/blackArrow.svg";
 // Dependencies
 import styled from "styled-components";
-// import ShowCase from "./Showcase";
+import ShowCase from "./Showcase";
 
 const Showcases = () => {
   return (
     <Parent>
-      {/* <ShowCase
+      <ShowCase
         title="APARTMENT"
         fill="#000"
         img={apartment1}
         label="Apartment"
         arrow1={blackArrow}
         arrow2={whiteArrow}
-      /> */}
-      {/* <ShowCase
+      />
+      <ShowCase
         title="PENTHOUSE"
         fill="#000"
         img={apartment2}
         label="Penthouse"
         arrow1={blackArrow}
         arrow2={whiteArrow}
-      /> */}
+      />
     </Parent>
   );
 };
